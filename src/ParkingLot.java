17e8508abd0034ddbce8352fc1490268e0295204
jjtheall <jtheall@quinnipiac.edu>
2022-04-03 16:01:09 -0400

    initial method testing, as well as creation of printSpots helper method:

--- src/ParkingLot.java
+++ src/ParkingLot.java
@@ -28,7 +28,31 @@ public class ParkingLot {
 	}
 
 	public void setTakenForTime(int time){
-		
+		//first, clear the lot
+		clearLot();
+		//we'll use this variable in the switch statement to determine
+		//how far to loop through spots array
+		int upperBound = 0;
+		//now switch on time param, looping through a
+		//percentage of spots array setting each spot to taken
+		switch(time){
+			case TIME_MORNING:
+				upperBound = (int) (.9 * spots.length);
+				System.out.println(upperBound);
+				break;
+			case TIME_NOON:
+				upperBound = (int) (.75 * spots.length);
+				System.out.println(upperBound);
+				break;
+			case TIME_AFTERNOON:
+				upperBound = (int) (.4 * spots.length);
+				System.out.println(upperBound);
+				break;
+			default:
+				System.out.println("not a valid time");
+				break;
+
+		}
 	}
 
 	//sets all spots in spots array to open
@@ -37,4 +61,12 @@ public class ParkingLot {
 			spots[i].setTaken(false);
 		}
 	}
+
+	//prints all spots in spots array
+	public void printSpots(){
+		for(int i=0; i<spots.length; i++){
+			System.out.println(spots[i]);
+		}
+	}
+
 }
